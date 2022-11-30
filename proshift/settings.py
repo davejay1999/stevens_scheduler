d85973085d3558690f1af708f4d1e98d7f0e1b0b
davejay1999 <davejay1999@gmail.com>
2022-11-30 18:41:22 -0500

    staticcc

--- proshift/settings.py
+++ proshift/settings.py
@@ -151,5 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 import os
+
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static") 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
