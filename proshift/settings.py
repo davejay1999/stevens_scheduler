44429e869cd4c33113689b2557aab533a79072f8
davejay1999 <davejay1999@gmail.com>
2022-12-01 03:36:52 -0500

    databases

--- proshift/settings.py
+++ proshift/settings.py
@@ -134,8 +134,12 @@ SWAGGER_SETTINGS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'yfkH6Jp6xahW1hkgd0fu',
+        'HOST': 'containers-us-west-103.railway.app',
+        'PORT': '8038',
     }
 }
 
